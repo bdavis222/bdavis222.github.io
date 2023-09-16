b2e7d405f7f44b42a30aa89ff56a77253260ff1e
Brian Davis <briandavis03@gmail.com>
2023-09-15 20:26:07 -0600

    Updated deployment files

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
 import "./styles.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Releases from "./components/Releases";
 import DotScanner from "./components/dotscanner/DotScanner";
 import PypiMaker from "./components/pypimaker/PypiMaker";
-import Operator from "./components/Operator";
-import Gravity from "./components/Gravity";
+import Operator from "./components/operator/Operator";
+import Gravity from "./components/gravity/Gravity";
 import Roadmap from "./components/roadmap/Roadmap";
 import PythonLanding from "./components/roadmap/PythonLanding";
 
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter basename={"/"}>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Releases />} />
           <Route path="/dotscanner" element={<DotScanner />} />
@@ -23,7 +23,7 @@ class App extends React.Component {
           <Route path="/roadmap" element={<Roadmap />} />
           <Route path="/python" element={<PythonLanding />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
