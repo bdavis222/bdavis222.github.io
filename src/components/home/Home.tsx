290a0576ed609f5f0f7d36ae390d695b4e8011e4
Brian Davis <24298180+bdavis222@users.noreply.github.com>
2023-12-09 14:55:53 -0700

    Update Home.tsx

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -146,19 +146,19 @@ function Home() {
       <h2>Publications</h2>
       <Publication
         title="A Recurrent Nova Super-Remnant in the Andromeda Galaxy"
-        journal="Nature"
+        journal="nature"
         date="Jan 2019"
         link="https://www.nature.com/articles/s41586-018-0825-4.epdf?author_access_token=Efv4kYyb8NweZ1W1tYmiYdRgN0jAjWel9jnR3ZoTv0N8qBJDnwa59hGY4PDdFI5yg_gnOrjBQgwM9FCrVQsuZahhXf1Qwl_lZHr5DDGM34vvvO_OQYNvo7VsvJMiCWIlCU1wcJOxs7-ZwFTtSFhguQ%3D%3D"
       />
       <Publication
         title="A Census of AHB Stars in Galactic Globular Clusters"
-        journal="The Astrophysical Journal"
+        journal="apj"
         date="Feb 2022"
         link="https://iopscience.iop.org/article/10.3847/1538-4357/ac4224"
       />
       <Publication
         title="The True Luminosities of Planetary Nebulae in M31’s Bulge"
-        journal="The Astrophysical Journal"
+        journal="apj"
         date="Aug 2018"
         link="https://iopscience.iop.org/article/10.3847/1538-4357/aad3c4"
       />
