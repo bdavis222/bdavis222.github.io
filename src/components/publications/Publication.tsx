4300f9e63ff0a6879a16b85eb6dfb5302a4a795c
Brian Davis <24298180+bdavis222@users.noreply.github.com>
2023-12-09 14:08:13 -0700

    Update Publication.tsx

--- src/components/publications/Publication.tsx
+++ src/components/publications/Publication.tsx
@@ -6,10 +6,24 @@ interface Props {
 }
 
 function Publication({ title, journal, date, link }: Props) {
+  const color = (function () {
+    switch (journal) {
+      case "Nature":
+        return { "green" };
+      case "The Astrophysical Journal":
+        return { "red" };
+      case "The Astronomical Journal":
+        return { "orange" };
+      case "Research Notes of the AAS":
+        return { "yellow" };
+      case "The Pennsylvania State University":
+        return { "blue" };
+    }
+  })();
   return (
     <div className="publication-container">
       <a href={link} target="_blank">
-        <div className="publication-card">
+        <div className="publication-card" style={{ backgroundColor: color }}>
           <p style={{ margin: 0, fontWeight: 800, fontSize: "medium" }}>
             {title}
           </p>
