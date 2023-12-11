4d73c81389ba400ddea4d2b722852ff46f95c8e8
Brian Davis <briandavis03@gmail.com>
2023-12-10 18:09:43 -0700

    Updated profile cards

--- src/components/profile-card/ProfileCard.tsx
+++ src/components/profile-card/ProfileCard.tsx
@@ -12,18 +12,11 @@ function ProfileCard({ type, title, link, icon, description }: Props) {
       <a href={link} target="_blank">
         <div className={type + "-card"}>
           <div className="profile-image-container">
-            <img
-              className="profile-image"
-              src="assets/headshot.png"
-              alt="Headshot photo"
-            />
+            <img className="profile-image" src={icon} alt="Headshot photo" />
           </div>
           <div className="profile-info-container">
             <p className="profile-name">{title}</p>
             <p className="profile-description">{description}</p>
-          </div>
-          <div className="profile-icon-container">
-            <img className="profile-icon" src={icon} alt="Icon" />
           </div>
         </div>
       </a>
