41a156aeacf37645348088dda72e81bb32361938
Brian Davis <24298180+bdavis222@users.noreply.github.com>
2024-01-07 18:39:36 -0700

    Update Python.tsx

--- src/components/roadmap/Python.tsx
+++ src/components/roadmap/Python.tsx
@@ -18,8 +18,10 @@ function Python() {
           introductory Python course
         </a>{" "}
         to bring you up to speed. If you already know how to program in either 
-        Python or any other language, you can skip to the next section. Note 
-        that a good understanding of object-oriented programming (OOP) is 
+        Python or any other language, you can skip to the next section.
+      </p>
+      <p>
+        Note that a good understanding of object-oriented programming (OOP) is 
         essential to success in software engineering. If you do not have a 
         solid grasp of OOP, please look over the lessons 
         on <span className="green-text">classes</span> and <span className="green-text">inheritance</span> in 
