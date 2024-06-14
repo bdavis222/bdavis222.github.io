4d2fe44ad9ad998205ec85cb2e57c652e75deabd
Brian Davis <briandavis03@gmail.com>
2024-06-14 15:37:39 -0600

    Moved lessons to public directory

--- src/components/python-course-mobile/PythonCourseMobile.tsx
+++ src/components/python-course-mobile/PythonCourseMobile.tsx
@@ -34,114 +34,70 @@ function PythonCourseMobile() {
         <h2 style={{ marginTop: 30 }}>Table of Contents</h2>
         <ol start={0}>
           <li>
-            <a href="src/components/python-course-mobile/lessons/00_intro.html">
-              Introduction
-            </a>
+            <a href="lessons/00_intro.html">Introduction</a>
           </li>
           <li>
-            <a href="src/components/python-course-mobile/lessons/01_basics.html">
-              Basics
-            </a>
+            <a href="lessons/01_basics.html">Basics</a>
           </li>
           <li>
-            <a href="src/components/python-course-mobile/lessons/02_tuples.html">
-              Tuples
-            </a>
+            <a href="lessons/02_tuples.html">Tuples</a>
           </li>
           <li>
-            <a href="src/components/python-course-mobile/lessons/03_lists.html">
-              Lists
-            </a>
+            <a href="lessons/03_lists.html">Lists</a>
           </li>
           <li>
-            <a href="src/components/python-course-mobile/lessons/04_conditionals.html">
-              Conditionals
-            </a>
+            <a href="lessons/04_conditionals.html">Conditionals</a>
           </li>
           <li>
-            <a href="src/components/python-course-mobile/lessons/05_while_loops.html">
-              While-loops
-            </a>
+            <a href="lessons/05_while_loops.html">While-loops</a>
           </li>
           <li>
-            <a href="src/components/python-course-mobile/lessons/06_for_loops.html">
-              For-loops
-            </a>
+            <a href="lessons/06_for_loops.html">For-loops</a>
           </li>
           <li>
-            <a href="src/components/python-course-mobile/lessons/07_sublists.html">
-              Sublists
-            </a>
+            <a href="lessons/07_sublists.html">Sublists</a>
           </li>
           <li>
-            <a href="src/components/python-course-mobile/lessons/08_functions.html">
-              Functions
-            </a>
+            <a href="lessons/08_functions.html">Functions</a>
           </li>
           <li>
-            <a href="src/components/python-course-mobile/lessons/09_documentation.html">
-              Documentation
-            </a>
+            <a href="lessons/09_documentation.html">Documentation</a>
           </li>
           <li>
-            <a href="src/components/python-course-mobile/lessons/10_files.html">
-              Files
-            </a>
+            <a href="lessons/10_files.html">Files</a>
           </li>
           <li>
-            <a href="src/components/python-course-mobile/lessons/11_sets.html">
-              Sets
-            </a>
+            <a href="lessons/11_sets.html">Sets</a>
           </li>
           <li>
-            <a href="src/components/python-course-mobile/lessons/12_dictionaries.html">
-              Dictionaries
-            </a>
+            <a href="lessons/12_dictionaries.html">Dictionaries</a>
           </li>
           <li>
-            <a href="src/components/python-course-mobile/lessons/13_classes.html">
-              Classes
-            </a>
+            <a href="lessons/13_classes.html">Classes</a>
           </li>
           <li>
-            <a href="src/components/python-course-mobile/lessons/14_inheritance.html">
-              Inheritance
-            </a>
+            <a href="lessons/14_inheritance.html">Inheritance</a>
           </li>
           <li>
-            <a href="src/components/python-course-mobile/lessons/15_error_handling.html">
-              Error handling
-            </a>
+            <a href="lessons/15_error_handling.html">Error handling</a>
           </li>
           <li>
-            <a href="src/components/python-course-mobile/lessons/16_modules.html">
-              Modules
-            </a>
+            <a href="lessons/16_modules.html">Modules</a>
           </li>
           <li>
-            <a href="src/components/python-course-mobile/lessons/17_numpy_arrays.html">
-              Numpy arrays
-            </a>
+            <a href="lessons/17_numpy_arrays.html">Numpy arrays</a>
           </li>
           <li>
-            <a href="src/components/python-course-mobile/lessons/18_plotting.html">
-              Plotting
-            </a>
+            <a href="lessons/18_plotting.html">Plotting</a>
           </li>
           <li>
-            <a href="src/components/python-course-mobile/lessons/19_nd_arrays.html">
-              N-dimensional arrays
-            </a>
+            <a href="lessons/19_nd_arrays.html">N-dimensional arrays</a>
           </li>
           <li>
-            <a href="src/components/python-course-mobile/lessons/20_images.html">
-              Images
-            </a>
+            <a href="lessons/20_images.html">Images</a>
           </li>
           <li>
-            <a href="src/components/python-course-mobile/lessons/21_modularization.html">
-              Modularization
-            </a>
+            <a href="lessons/21_modularization.html">Modularization</a>
           </li>
         </ol>
         <p>
