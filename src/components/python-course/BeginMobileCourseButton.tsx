c83dcf63bdd8dfcfe8976ef6b784deaaa900fc5c
Brian Davis <briandavis03@gmail.com>
2024-06-14 15:41:04 -0600

    Updated link for mobile course launch button

--- src/components/python-course/BeginMobileCourseButton.tsx
+++ src/components/python-course/BeginMobileCourseButton.tsx
@@ -1,10 +1,7 @@
 function BeginMobileCourseButton() {
   return (
     <div className="python-course-button-container">
-      <a
-        href="src/components/python-course-mobile/lessons/00_intro.html"
-        className="python-course-button green"
-      >
+      <a href="lessons/00_intro.html" className="python-course-button green">
         Begin course
       </a>
     </div>
