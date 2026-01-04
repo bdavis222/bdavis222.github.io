b6477a46a7534d4c493c31a12c989b426b0a03a0
Brian Davis <24298180+bdavis222@users.noreply.github.com>
2026-01-04 12:05:39 -0600

    Updated the title text of the Operator mobile app store buttons

--- src/components/operator/MobileAppStoreButtons.tsx
+++ src/components/operator/MobileAppStoreButtons.tsx
@@ -12,12 +12,14 @@ function MobileAppStoreButtons({ iosLink, androidLink }: Props) {
         url={iosLink}
         theme={"dark"}
         className={"custom-app-store-btn"}
+        title="Download for free!"
       />
       <GooglePlayButton
         url={androidLink}
         theme={"dark"}
         className={"custom-google-play-btn"}
         width={200}
+        title="Download for free!"
       />
     </div>
   );
